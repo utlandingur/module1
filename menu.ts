1d764a288bce30a5057b28239ff99838f96be15e
Luke Hening <152434106+utlandingur@users.noreply.github.com>
2024-08-28 15:36:49 +0000

    Add nav styling

--- menu.ts
+++ menu.ts
@@ -3,25 +3,45 @@
 
 // Grab the div which contains the menu overlay (it is set to none by default as is therefore hidden)
 
-const menu = document.querySelector(".menu-overlay") as HTMLDivElement;
+const overlay = document.querySelector(
+  ".menu-overlay"
+) as HTMLDivElement | null;
 
-// Grab the hamburger icons (we'll have two, one on nav bar and one on the menu overlay)
-// We use querySelectorAll because there are multiple elements with the class hamburger
-// We cast the result to a NodeListOf<HTMLDivElement> because we know the elements are divs
+const navBar = document.querySelector(".nav-bar") as HTMLDivElement | null;
 
-const hamburgers = document.querySelectorAll(
+// Grab the hamburger icon
+const hamburger = document.querySelector(
   ".hamburger"
-) as NodeListOf<HTMLDivElement>;
+) as HTMLImageElement | null;
 
 // For each hamburger icon, add an event listener, so we know when it is clicked
 
-hamburgers?.forEach((hamburger) => {
-  hamburger.addEventListener("click", () => {
-    // If the menu is displayed, set the display to none, otherwise set it to block
-    // This will toggle the display of the menu overlay
+hamburger?.addEventListener("click", () => {
+  // If the menu is displayed, set the display to none, otherwise set it to block
+  // This will toggle the display of the menu overlay
 
-    menu.style.display === "none"
-      ? (menu.style.display = "block")
-      : (menu.style.display = "none");
-  });
+  if (overlay === null) return;
+
+  if (overlay.style.display === "none" || overlay.style.display === "") {
+    overlay.style.display = "block";
+    overlay.classList.remove("hide");
+    overlay.classList.add("show");
+    if (navBar) {
+      navBar.style.backgroundColor = "transparent";
+      navBar.style.backdropFilter = "none";
+    }
+    document.body.style.overflowY = "hidden"; // Prevent scrolling when the menu is open
+  } else {
+    overlay.classList.remove("show");
+    overlay.classList.add("hide");
+    document.body.style.overflowY = "auto"; // Allow scrolling when the menu is closed
+    if (navBar) {
+      navBar.style.backgroundColor = "rgba(255, 208, 0, 0.86)";
+      navBar.style.backdropFilter = "blur(10px)";
+    }
+    // Wait for the animation to finish before setting the display
+    setTimeout(() => {
+      overlay.style.display = "none";
+    }, 500);
+  }
 });
